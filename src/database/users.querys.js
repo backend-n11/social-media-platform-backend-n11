301049f2f7c4f18591bc79025b96e3624e7000b1
oybekjon08 <uzbbnbf@gmail.com>
2024-05-18 11:44:14 +0500

    names updated

--- src/database/users.querys.js
+++ src/database/users.querys.js
@@ -1,15 +1,21 @@
 export function selectAllUsers(){
     const query = `SELECT * FROM users`
+    return query
 }
 
 export function selectOneUser(){
     const query = `SELECT * FROM users WHERE id = $1`
+    return query
+
 }
 
-export function update_user(){
+export function updateUser(){
     const query = `UPDATE users set username = $1, email = $2, password = $3, gender = $4`
+    return query
+
 }
 
-export function delete_user(){
+export function deleteUser(){
     const query = 'DELETE FROM users where id = $1'
+    return query
 }
